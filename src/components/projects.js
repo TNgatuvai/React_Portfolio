33b494a6d33df5b3029defb51219b6a7ffc31ff7
Tali Ngatuvai <Tngatuvai85@gmail.com>
2020-08-06 01:01:29 -0600

    Getting routes in place

---
+++ src/components/projects.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render() {
+        <div><h1>Projects Page</h1></div>
+       )
+    }
+}
+
+export default Projects;
---
+++ src/components/resume.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+    render() {
+        <div><h1>Resume Page</h1></div>
+       )
+    }
+}
+
+export default Resume;
